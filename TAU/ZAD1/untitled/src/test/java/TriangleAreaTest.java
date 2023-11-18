b864d6488d989fb30d852dd411d1512cc319523e
s23578-pj <73029891+s23578-pj@users.noreply.github.com>
2023-11-18 16:59:40 +0100

    Add files via upload

--- TAU/ZAD1/untitled/src/test/java/TriangleAreaTest.java
+++ TAU/ZAD1/untitled/src/test/java/TriangleAreaTest.java
@@ -11,18 +11,18 @@ public class TriangleAreaTest {
         assertTrue(TriangleArea.canBuildTriangle(5, 12, 13));
     }
 
-//    @Test
-//    public void testCannotBuildTriangleWithInvalidSides() {
-//        assertFalse(TriangleArea.canBuildTriangle(1, 1, 3));
-//        assertFalse(TriangleArea.canBuildTriangle(0, 0, 0));
-//    }
+    @Test
+    public void testCannotBuildTriangleWithInvalidSides() {
+        assertFalse(TriangleArea.canBuildTriangle(1, 1, 3));
+        assertFalse(TriangleArea.canBuildTriangle(0, 0, 0));
+    }
 
-//    @Test
-//    public void testCalculateTriangleArea() {
-//        assertEquals(6.0, TriangleArea.calculateTriangleArea(3, 4, 5), 0.001);
-//        assertEquals(84.0, TriangleArea.calculateTriangleArea(7, 24, 25), 0.001);
-//        assertEquals(30.0, TriangleArea.calculateTriangleArea(5, 12, 13), 0.001);
-//    }
+    @Test
+    public void testCalculateTriangleArea() {
+        assertEquals(6.0, TriangleArea.calculateTriangleArea(3, 4, 5), 0.001);
+        assertEquals(84.0, TriangleArea.calculateTriangleArea(7, 24, 25), 0.001);
+        assertEquals(30.0, TriangleArea.calculateTriangleArea(5, 12, 13), 0.001);
+    }
 
     @Test
     public void testInvalidTriangleArea() {
@@ -30,25 +30,25 @@ public class TriangleAreaTest {
         assertTrue(Double.isNaN(invalidArea));
     }
 
-//    @Test
-//    public void testInvalidSides() {
-//        assertFalse(TriangleArea.canBuildTriangle(-1, 2, 3));
-//        assertFalse(TriangleArea.canBuildTriangle(1, -2, 3));
-//        assertFalse(TriangleArea.canBuildTriangle(1, 2, -3));
-//    }
+    @Test
+    public void testInvalidSides() {
+        assertFalse(TriangleArea.canBuildTriangle(-1, 2, 3));
+        assertFalse(TriangleArea.canBuildTriangle(1, -2, 3));
+        assertFalse(TriangleArea.canBuildTriangle(1, 2, -3));
+    }
 
-//    @Test
-//    public void testZeroSides() {
-//        assertFalse(TriangleArea.canBuildTriangle(0, 1, 2));
-//        assertFalse(TriangleArea.canBuildTriangle(1, 0, 2));
-//        assertFalse(TriangleArea.canBuildTriangle(1, 2, 0));
-//    }
+    @Test
+    public void testZeroSides() {
+        assertFalse(TriangleArea.canBuildTriangle(0, 1, 2));
+        assertFalse(TriangleArea.canBuildTriangle(1, 0, 2));
+        assertFalse(TriangleArea.canBuildTriangle(1, 2, 0));
+    }
 
-//    @Test
-//    public void testEquilateralTriangle() {
-//        assertTrue(TriangleArea.canBuildTriangle(4, 4, 4));
-//        assertEquals(6.928, TriangleArea.calculateTriangleArea(4, 4, 4), 0.001);
-//    }
+    @Test
+    public void testEquilateralTriangle() {
+        assertTrue(TriangleArea.canBuildTriangle(4, 4, 4));
+        assertEquals(6.928, TriangleArea.calculateTriangleArea(4, 4, 4), 0.001);
+    }
 
     @Test
     public void testIsoscelesTriangle() {
